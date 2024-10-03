d49be80f9a2c59126f0781274c5acff3ad8cca32
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-10-03 22:39:46 +0300

    update

---
+++ Pytorch/DataLoader.py
@@ -0,0 +1,7 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from matplotlib.pyplot import plt
+
+train_data = dataset.FashionMNIST
